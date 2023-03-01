afcfd1dc862da1ea549760e2c07d6d4840bb4c75
Mike Steinmetz <mikesteinmetz00@gmail.com>
2023-02-28 20:16:00 -0600

    getting onClick to generate a random color inside shape

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-var el_up = document.getElementById("");
-var el_down = document.getElementById("");
-
-    function changeColor(color) {
-        document.body.style.background = color;
-    }
-
-    function gfg_Run() {
-        changeColor('');
-        el_down.innerHTML = "Background Color changed!";
+    function changeColor() {
+        let color = '#';
+        color += random = Math.random().toString(16).slice(2,8);
+        console.log(color);
+        document.querySelector('.shape').style.backgroundColor = color;
     }
